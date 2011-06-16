903d5b525a6918933fa6a4067895cf84c20b76d8
ilya <ilya@ilyas-iMac.local>
2011-06-16 17:14:26 +0400

    Initial

--- log2ban.py
+++ log2ban.py
@@ -12,16 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""log2ban.py
-
-    *** About
+"""
+    #About
 
     This program allows detection and banning of IPs participating in DDOS or bruteforce attack to the webserver.
 
     Such kind of attack is characterized by a high number of similar requests from a relatively small subset of IPs in
     a short period of time.
 
-    *** How log2ban works
+    #How log2ban works
 
     For detection, each request to the server is marked with an identifier made from request properties (for example,
     from IP and request URL: "1.2.3.4/login.php"). When the number of hits with a certain ID reaches tolerance
@@ -35,7 +34,7 @@
     configuration. If command sends EOF, log2ban will terminate. If command will stop writing log records to the
     stdout, log2ban will hang forever.
 
-    *** Tuning
+    #Tuning
 
     Default Apache/nginx access log pattern is supported from the box. Modifications made to the default format must be
     reflected in ACCESS_LOG_RECORD_FORMAT variable.   
@@ -48,27 +47,32 @@
     To for record skipping criteria, modify "skip" function. Currently, requests to several static file types are
     skipped from processing.
 
-    *** Blocking Lists
+    #Blocking Lists
 
     To use blocking lists, please enable the DB, process some log records, and then execute
 
-    # python log2ban.py print (banned | allbanned)
+    > python log2ban.py print (banned | allbanned)
 
     to print collected IPs to stdout. Specifying "banned" will print only new IPs, while "allbanned" will print every
     banned IP, no matter was it previously printed or not. 
 
     After the few days the you may wish to unban IPs:
 
-    # python log2ban.py print unbanned
+    > python log2ban.py print unbanned
 
     This command will print every IP banned before current_time - DAYS_UNBAN, and remove those ip from the DB.
 
-    *** Performance
+    # Performance
 
     log2ban is enough fast by itself, but for a very high number of requests CPU usage may become a problem. Consider
     disabling logging of requests to static resources, such as images, scripts or style sheets. Further optimization
     may include using simpler access log format (csv) instead of using the default one, which is parsed by complicated
     apachelog's algorythm based on regular expressions.
+
+    # Contacts
+
+    Developer: [unicodefreak@gmail.com](mailto:unicodefreak@gmail.com)
+
 
 """
 
